a7589065aba615050858d48ae3b1e8a1588cfb59
abdul mudhir <abdulmudhirss@gmail.com>
2020-08-01 20:07:22 +0100

    Initial commit

---
+++ GymFit/Components/Navigation.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymFit.Components
+{
+    public class Navigation:ViewComponent
+    {
+
+
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+
+
+        }
+    }
+}
