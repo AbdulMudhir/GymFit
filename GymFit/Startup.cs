f0f3643f48b0205a93e3b869f59c77555e2f9df5
abdul mudhir <abdulmudhirss@gmail.com>
2020-08-02 21:40:00 +0100

    navigation created aswell as search

--- GymFit/Startup.cs
+++ GymFit/Startup.cs
@@ -17,8 +17,8 @@ namespace GymFit
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-           
 
+            services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddControllersWithViews().AddJsonOptions(opts => opts.JsonSerializerOptions.PropertyNamingPolicy = null);
             services.AddScoped<IImageRepository, MockImageRepository>();
 
